488915a0ea2003c72231bd9a85849734e53588d3
Erick Lasluisa <erickl1709@gmail.com>
2024-05-18 21:04:45 -0500

    actualizacion hero, informacion y navbar

--- src/App.js
+++ src/App.js
@@ -1,53 +1,50 @@
-import React, { useState } from 'react'
-import Nav from './Components/Nav'
-import About from './Components/About'
-import Services from './Components/Services'
-import Client from './Components/Client'
-import Gallery from './Components/Gallery'
-import TeamMembers from './Components/TeamMembers'
-import TimeSchedule from './Components/TimeSchedule'
-import Footer from './Components/Footer'
-import HeroSection from './Components/HeroSection'
-import { ContactUs } from './Components/ContactUs'
-import Subscription from './Components/Subscription'
+import React, { useState } from "react";
+import Nav from "./Components/Nav";
+import About from "./Components/About";
+import Services from "./Components/Services";
+import Client from "./Components/Client";
+import Gallery from "./Components/Gallery";
+import TeamMembers from "./Components/TeamMembers";
+import TimeSchedule from "./Components/TimeSchedule";
+import Footer from "./Components/Footer";
+import HeroSection from "./Components/HeroSection";
+import { ContactUs } from "./Components/ContactUs";
+import Subscription from "./Components/Subscription";
 
 const App = () => {
-
-  
-  const [nav,setNav] = useState(false)
+  const [nav, setNav] = useState(false);
 
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const toggleModal = () => {
     setIsModalOpen(!isModalOpen);
-    console.log(isModalOpen)
+    console.log(isModalOpen);
   };
 
-  window.addEventListener("scroll",()=>{
-    const scroll = document.documentElement.scrollTop
-    if(scroll > 405){
-      setNav(true)
+  window.addEventListener("scroll", () => {
+    const scroll = document.documentElement.scrollTop;
+    if (scroll > 405) {
+      setNav(true);
+    } else {
+      setNav(false);
     }
-    else{
-      setNav(false)
-    }
-   })
+  });
 
   return (
-    <div className='App'>
-        <Nav nav={nav} toggleModal={toggleModal}/>
-        <HeroSection />
-        <About />
-        <Services />
-        <Client />
-        <Gallery />
-        <TeamMembers />
-        <Subscription isModalOpen={isModalOpen} toggleModal={toggleModal}/>
-        <TimeSchedule />
-        <ContactUs />
-        <Footer nav={nav}/>
+    <div className="App">
+      <Nav nav={nav} toggleModal={toggleModal} />
+      <HeroSection toggleModal={toggleModal} />
+      <About />
+      <Services />
+      <Client />
+      <Gallery />
+      <TeamMembers />
+      <Subscription isModalOpen={isModalOpen} toggleModal={toggleModal} />
+      <TimeSchedule />
+      <ContactUs />
+      <Footer nav={nav} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
